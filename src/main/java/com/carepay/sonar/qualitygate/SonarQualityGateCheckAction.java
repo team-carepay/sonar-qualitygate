0ef3cf0fbfa7ea88ec567f4c9001b7dc2b228cbe
Barry Lagerweij <b.lagerweij@carepay.co.ke>
2021-05-21 23:16:50 +0200

    Added javadoc, support other sonarqube server url, support password

--- src/main/java/com/carepay/sonar/qualitygate/SonarQualityGateCheckAction.java
+++ src/main/java/com/carepay/sonar/qualitygate/SonarQualityGateCheckAction.java
@@ -13,11 +13,15 @@ import com.github.cliftonlabs.json_simple.JsonObject;
 import org.gradle.api.Action;
 import org.gradle.api.Task;
 
+/**
+ * Action in Gradle which runs after the SonarQube task has finished.
+ * Waits for the analysis to be completed, then verifies the quality-gate result.
+ */
 public class SonarQualityGateCheckAction implements Action<Task> {
     private static final int NR_OF_RETRIES = 30;
     private static final long WAIT_TIME_MILLIS = 5000L;
     private static final String CE_TASK_URL_PROPERTY = "ceTaskUrl";
-    private static final String PROJECT_STATUS_URL = "https://sonarcloud.io/api/qualitygates/project_status?analysisId=";
+    private static final String PROJECT_STATUS_URL = "/api/qualitygates/project_status?analysisId=";
     private static final String SUCCESS_STATUS = "SUCCESS";
 
     private final ResourceFetcher resourceFetcher;
@@ -30,6 +34,11 @@ public class SonarQualityGateCheckAction implements Action<Task> {
         this.resourceFetcher = resourceFetcher;
     }
 
+    /**
+     * Executes the quality-gate task. Expects the sonar.login to be defined in the properties.
+     * @param task the SonarQube task
+     * @throws IllegalStateException in case of any problems
+     */
     @Override
     public void execute(Task task) {
         @SuppressWarnings("unchecked")
@@ -37,18 +46,28 @@ public class SonarQualityGateCheckAction implements Action<Task> {
         if (properties == null) {
             throw new IllegalStateException("Sonarqube task properties missing");
         }
-        final String token = String.valueOf(properties.get("sonar.login"));
+        final String hostUrl = (String)properties.getOrDefault("sonar.host.url", "https://sonarcloud.io");
+        final String login = (String)properties.get("sonar.login");
+        final String password = (String)properties.getOrDefault("sonar.password", "");
         task.getLogger().info("checking quality gate");
         final Properties props = getTaskProperties(new File(task.getProject().getBuildDir(), "sonar/report-task.txt"));
-        final String analysisId = waitForAnalysisId(props.getProperty(CE_TASK_URL_PROPERTY), token);
+        final String analysisId = waitForAnalysisId(props.getProperty(CE_TASK_URL_PROPERTY), login, password);
         task.getLogger().info("analysis id {}", analysisId);
-        final String status = getQualityGateStatus(analysisId, token);
+        final URL analysisUrl = URLOpener.create(hostUrl + PROJECT_STATUS_URL + analysisId);
+        final String status = getQualityGateStatus(analysisUrl, login, password);
         task.getLogger().info("quality gate project status is {}", status);
         if (!"OK".equals(status)) {
             throw new IllegalStateException("Quality gate failed: " + status);
         }
     }
 
+    /**
+     * Extracts the content of the SonarQube report-task file
+     *
+     * @param reportFile the sonarqube report-task.txt file
+     * @return the contents of the file as a Properties object
+     * @throws IllegalStateException in case the file cannot be found or parsed
+     */
     protected Properties getTaskProperties(final File reportFile) {
         if (!reportFile.exists()) {
             throw new IllegalStateException("Report file not found: " + reportFile);
@@ -62,9 +81,16 @@ public class SonarQualityGateCheckAction implements Action<Task> {
         return props;
     }
 
-    protected String getQualityGateStatus(final String analysisId, final String sonarToken) {
-        final URL url = URLOpener.create(PROJECT_STATUS_URL + analysisId);
-        final JsonObject responseJson = resourceFetcher.queryJsonBasicAuth(url, sonarToken, "");
+    /**
+     * Extracts the analysis project status from the Sonar server
+     * @param analysisUrl url of the analysis result
+     * @param login sonar login
+     * @param password sonar password
+     * @return the status
+     * @throws IllegalStateException in case the JSON is invalid
+     */
+    protected String getQualityGateStatus(final URL analysisUrl, final String login, final String password) {
+        final JsonObject responseJson = resourceFetcher.queryJsonBasicAuth(analysisUrl, login, password);
         final JsonObject statusJson = responseJson.getMap(ProjectStatusJsonProperty.projectStatus);
         if (statusJson == null) {
             throw new IllegalStateException("Invalid JSON response: missing 'projectStatus'");
@@ -72,11 +98,18 @@ public class SonarQualityGateCheckAction implements Action<Task> {
         return statusJson.getString(ProjectStatusJsonProperty.status);
     }
 
-    protected String waitForAnalysisId(final String taskUrlString, final String sonarToken) {
+    /**
+     * Waits for the analysis to be completed
+     * @param taskUrlString url of the task result
+     * @param login sonar login
+     * @param password sonar password
+     * @return analysisId of the completed task
+     */
+    protected String waitForAnalysisId(final String taskUrlString, final String login, final String password) {
         final URL ceTaskUrl = URLOpener.create(taskUrlString);
         try {
             for (int i = 0; i < NR_OF_RETRIES; i++) {
-                final JsonObject responseJson = resourceFetcher.queryJsonBasicAuth(ceTaskUrl, sonarToken, "");
+                final JsonObject responseJson = resourceFetcher.queryJsonBasicAuth(ceTaskUrl, login, password);
                 final JsonObject taskJson = responseJson.getMap(TaskJsonProperty.task);
                 if (taskJson == null) {
                     throw new IllegalStateException("Invalid JSON response: missing 'task'");
